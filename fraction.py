b8afb1c17cb3f90712c7c91cf0fb2673a754d3d1
Anant Arayanant <minecraftraguna@gmail.com>
2019-08-20 13:52:03 +0700

    add __add__ and its testcase

--- fraction.py
+++ fraction.py
@@ -14,16 +14,19 @@ class Fraction:
         """Initialize a new fraction with the given numerator
            and denominator (default 1).
         """
-        gcd = math.gcd(numerator,denominator)
-        if denominator < 0:
-            numerator *= -1
-            denominator *= -1
-        self.numerator = int(numerator/gcd)
-        self.denominator = int(denominator/gcd)
+        try:
+            gcd = math.gcd(numerator,denominator)
+            if denominator < 0:
+                numerator *= -1
+                denominator *= -1
+            self.numerator = int(numerator/gcd)
+            self.denominator = int(denominator/gcd)
+        except:
+            self.numerator = numerator
+            self.denominator = denominator
+
 
     def __str__(self):
-        if self.denominator == 0:
-            return 'Fraction is undefined.'
         if self.denominator == 1 or self.numerator == 0:
             return '{0}'.format(int(self.numerator/self.denominator))
         return '{0}/{1}'.format(self.numerator, self.denominator)
@@ -33,7 +36,9 @@ class Fraction:
         """Return the sum of two fractions as a new fraction.
            Use the standard formula  a/b + c/d = (ad+bc)/(b*d)
         """
-        pass
+        new_numerator = self.numerator * frac.denominator + frac.numerator * self.denominator
+        new_denominator = self.denominator * frac.denominator
+        return Fraction(new_numerator, new_denominator)
 
     #TODO write __mul__ and __str__.  Verify __eq__ works with your code.
     #Optional have fun and overload other operators such as 
@@ -46,6 +51,5 @@ class Fraction:
            Fractions are stored in proper form so the internal representation
            is unique (3/6 is same as 1/2).
         """
-        if self.denominator == 0 and frac.denominator == 0:
-            return True
         return self.numerator == frac.numerator and self.denominator == frac.denominator
+
--- fraction_test.py
+++ fraction_test.py
@@ -27,10 +27,10 @@ class FractionTest(unittest.TestCase):
         # Constructor should provide default denominator = 1
         f = Fraction(99)
         self.assertEqual("99", f.__str__())
-        f = Fraction(10,0)
-        self.assertEqual("Fraction is undefined.", f.__str__())
-        f = Fraction(-50,0)
-        self.assertEqual("Fraction is undefined.", f.__str__())
+        f = Fraction(10, 0)
+        self.assertEqual("1/0", f.__str__())
+        f = Fraction(-50, 0)
+        self.assertEqual("-1/0", f.__str__())
 
 
     # TODO Write tests for __init__, __eq__, +, *.
@@ -38,26 +38,38 @@ class FractionTest(unittest.TestCase):
     # The test requires that your __eq__ is correct.
     def test_add(self):
         # 3/4 = 2/3 + 1/12
-        self.assertEqual(Fraction(3,4), Fraction(1,12)+Fraction(2,3))
+        self.assertEqual(Fraction(3, 4), Fraction(1, 12) + Fraction(2, 3))
+        # 1/2 = 6/8 + (-1/4)
+        self.assertEqual(Fraction(1, 2), Fraction(6, 8) + Fraction(-1, 4))
+        # -4/5 = (-1/10) + (-3/10) + (-2/5)
+        self.assertEqual(Fraction(-4, 5), Fraction(-1, 10) + Fraction(-3, 10) + Fraction(-2, 5))
+        # 1/3 = 2/6 + 0/5
+        self.assertEqual(Fraction(1, 3), Fraction(2, 6) + Fraction(0, 5))
 
     def test_eq(self):
-        f = Fraction(1,2)
-        g = Fraction(-40,-80)
-        h = Fraction(10000,20001) # not quite 1/2
+        f = Fraction(1, 2)
+        g = Fraction(-40, -80)
+        h = Fraction(10000, 20001) # not quite 1/2
         self.assertTrue(f == g)
         self.assertTrue(f.__eq__(g))  # same thing
         self.assertFalse(f == h)
         self.assertFalse(f.__eq__(h))
-        f = Fraction(-3,4)
-        g = Fraction(15,-20)
-        h = Fraction(-9000,-12000)
+        f = Fraction(-3, 4)
+        g = Fraction(15, -20)
+        h = Fraction(9000, -11999)
         self.assertTrue(f == g)
         self.assertFalse(f == h)
-        f = Fraction(0,10)
-        g = Fraction(0,-1000)
-        h = Fraction(10,0)
-        i = Fraction(-1000,0)
+        f = Fraction(0, 10)
+        g = Fraction(0, -1000)
+        self.assertTrue(f == g)
+        self.assertFalse(f == h)
+        f = Fraction(10, 0)
+        g = Fraction(100, 0)
+        h = Fraction(-500, 0)
+        i = Fraction(-1000, 0)
         self.assertTrue(f == g)
         self.assertFalse(f == h)
         self.assertTrue(h == i)
-        self.assertFalse(g == i)
+        f = Fraction(0, 0)
+        g = Fraction(0, 0)
+        self.assertTrue(f == g)
